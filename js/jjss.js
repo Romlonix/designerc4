1f2bb2fa669a26fcfe97ea6aebf0ecab05b5968a
Romlonix <descarga7@hotmail.com>
2020-11-03 13:13:39 -0600

    v1, Estable

---
+++ js/jjss.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-nav").onclick = function () {
+
+  document.getElementById("btn-nav").classList.toggle("fa-bars");
+  document.getElementById("btn-nav").classList.toggle("fa-times");
+
+  document.getElementById("nav-dropdown").classList.toggle("visibility-visible");
+
+}
